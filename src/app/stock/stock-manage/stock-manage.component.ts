4ea47ec4a7be000691290d27712d524743ff17ee
gritman <gritman@126.com>
2017-05-21 17:50:06 +0800

    实现stock-manage组件的控制器,股票类

--- src/app/stock/stock-manage/stock-manage.component.ts
+++ src/app/stock/stock-manage/stock-manage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-stock-manage',
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockManageComponent implements OnInit {
 
-  constructor() { }
+  private stocks: Array<Stock>;
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.stocks = [
+      new Stock(1, '股票名1', 1.99, 3.5, '股票说明1', ['饮料', '食品']),
+      new Stock(2, '股票名2', 5.37, 4.5, '股票说明2', ['IT', '互联网']),
+      new Stock(3, '股票名3', 3.37, 2.5, '股票说明3', ['制造', '互联网']),
+      new Stock(4, '股票名4', 4.37, 3.5, '股票说明4', ['IT', '消费']),
+      new Stock(5, '股票名5', 5.37, 4.5, '股票说明5', ['金融', '互联网']),
+      new Stock(6, '股票名6', 6.37, 2.0, '股票说明6', ['金融', '消费']),
+      new Stock(7, '股票名7', 7.37, 4.0, '股票说明7', ['IT', '互联网']),
+      new Stock(8, '股票名8', 8.37, 3.0, '股票说明8', ['制造', '食品']),
+    ];
+  }
+
+}
+
+export class Stock {
+  constructor(public id: number,
+              public name: string,
+              public price: number,
+              public rating: number,
+              public desc: string,
+              public categories: Array<string>) {
+
+  }
 }
