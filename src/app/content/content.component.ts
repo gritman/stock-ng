4cc64d66096ecb603a4950ca35571cc6bc919e33
gritman <gritman@126.com>
2017-05-28 01:09:38 +0800

    根据不同的路由显示不同的页面标题

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit} from "@angular/core";
+import {NavigationEnd, Router} from "@angular/router";
+import 'rxjs/add/operator/filter';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
@@ -7,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
-  constructor() { }
+  private pageTitle = '';
+  private pageDesc = '';
+
+  constructor(public router: Router) {
+    router.events
+      .filter(event => event instanceof NavigationEnd)
+      .subscribe((event: NavigationEnd) => {
+        if (event.url == '/dashboard') {
+          this.pageTitle = '这里是首页';
+          this.pageDesc = '首页描述';
+        } else if (event.url.startsWith('/stock')) {
+          this.pageTitle = '股票信息管理';
+          this.pageDesc = '进行股票基本信息增删改查';
+        }
+      });
+  }
 
   ngOnInit() {
   }
