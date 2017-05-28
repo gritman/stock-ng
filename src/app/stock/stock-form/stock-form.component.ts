3661388c7c175ab11806a7abac429ea7e9a20507
gritman <gritman@126.com>
2017-05-28 19:38:33 +0800

    完善股票表单页面,变量绑定到组件

--- src/app/stock/stock-form/stock-form.component.ts
+++ src/app/stock/stock-form/stock-form.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Stock, StockService} from '../stock.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-stock-form',
@@ -11,11 +11,21 @@ export class StockFormComponent implements OnInit {
 
   stock: Stock;
 
-  constructor(private routeInfo: ActivatedRoute, private stockService: StockService) { }
+  constructor(private routeInfo: ActivatedRoute,
+              private stockService: StockService,
+              private router: Router) {
+  }
 
   ngOnInit() {
     const stockId = this.routeInfo.snapshot.params['id'];
     this.stock = this.stockService.getStock(stockId);
   }
 
+  cancel() {
+    this.router.navigateByUrl('/stock');
+  }
+
+  save() {
+    this.router.navigateByUrl('/stock');
+  }
 }
