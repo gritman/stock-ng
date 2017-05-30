1e74a2f08ffcbbb0b24dbd900fa320534748f0d6
gritman <gritman@126.com>
2017-05-30 22:50:57 +0800

    把星级组件的加星逻辑处理移动到ngOnChanges中

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,11 +1,11 @@
-import {Component, OnInit, Input, EventEmitter, Output} from '@angular/core';
+import {Component, OnInit, Input, EventEmitter, Output, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit, OnChanges {
 
   @Input()
   private rating = 0;
@@ -22,6 +22,9 @@ export class StarsComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
     // true代表是空心星星,false代表实心星星
     // this.stars = [false, true, false, false, false];
     this.stars = []; // 必须先初始化
@@ -33,10 +36,6 @@ export class StarsComponent implements OnInit {
   clickStar(index: number) {
     if (!this.readonly) {
       this.rating = index + 1;
-      this.stars = []; // 必须先初始化
-      for (let i = 1; i <= 5; ++i) {
-        this.stars.push(i > this.rating);
-      }
       this.ratingChange.emit(this.rating);
     }
   }
