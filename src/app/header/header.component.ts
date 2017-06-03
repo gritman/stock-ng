300b3a78657c06df135b98999369cf7280f08ee6
gritman <gritman@126.com>
2017-06-04 00:48:25 +0800

    多环境配置environment,根据启动命令的参数决定

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {WebSocketService} from '../shared/web-socket.service';
+import {environment} from '../../environments/environment'; // 记住,要引用environment.ts,而不是environment.prod.ts
 
 @Component({
   selector: 'app-header',
@@ -9,6 +10,7 @@ import {WebSocketService} from '../shared/web-socket.service';
 export class HeaderComponent implements OnInit {
 
   messageCount = 0;
+  title = environment.appTitle;
 
   constructor(public websocketService: WebSocketService) {
 
